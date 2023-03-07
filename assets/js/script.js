552b19b3893f3e32ee79a70860a046964c94034b
Jamie Nadel <jamienadel4@gmail.com>
2023-03-06 21:55:46 -0500

    added random drink and recipe api calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
 console.log('hello');
 
-console.log('random drink branch test');
+function getRandomDrink() {
+  var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+  
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json().then(function(data) {
+        console.log('random drink', data);
+      })
+    })
+};
+
+getRandomDrink();
+
+function getRandomRecipe() {
+  var requestRecipe = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+  fetch(requestRecipe)
+    .then(function (response) {
+      return response.json().then(function(data) {
+        console.log('random recipe', data);
+      })
+    })
+};
+
+getRandomRecipe();
+
+
+
